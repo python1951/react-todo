86a7196bb2485d5d1beb19477672ca1b0bcf3fdd
=Awais Qamar <qamarha28@gmail.com>
2021-08-05 12:22:38 +0500

    react-todo-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button,Input,InputLabel, FormControl} from '@material-ui/core';
+import {useState} from 'react';
 
 function App() {
+const [todos, settodos] = useState(['Fetch Groceries from shop!',' Take the family for Dinner!','Read the Documentary on Great Leaders!'])
+const [input, setinput] = useState('');
+console.log(input);
+const addTodo = (event) =>{
+    event.preventDefault();
+    settodos([...todos,input]);
+    setinput('');
+    console.log(todos)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+        Hello Programmers!!!
+
+     </h1>
+     <FormControl>
+       <InputLabel>Your Day Todos</InputLabel>
+     <Input value={input} onChange={event => setinput(event.target.value)}/>
+     <Button disabled ={!input} type='submit' onClick={addTodo} variant="contained" color="primary">
+     Add Todo
+     </Button>
+     </FormControl>
+     
+     
+     {todos.map(todo =>(
+       <ul>
+         {todo}
+         </ul>
+     ))}
+     
     </div>
   );
 }
